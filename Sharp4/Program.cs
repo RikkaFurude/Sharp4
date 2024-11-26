a59ed181985893207d6d332478c953ceae974ff8
Comp <Comp@PENDULUM>
2024-11-27 01:46:23 +0500

    Добавьте файлы проекта.

---
+++ Sharp4/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using static System.Net.WebRequestMethods;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Serialization;
+
+
+
+
+namespace Sharp4
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            string DZ;
+            Console.Write("Введите задание: ");
+            DZ = Console.ReadLine();
+            switch (DZ)
+            {
+
+                //1
+                case ("1"):
+                    List<int> list1 = new List<int> { 1,6,7,15,19 };
+                    List<int> list2 = new List<int> { };
+                    list1 = collections.One(list1, list2);
+                    for (int i = 0; i < list1.Count; i++)
+                        Console.Write(list1[i] + " ");
+                    Console.WriteLine();
+                    break;
+                //2
+                case ("2"):
+                    LinkedList<int> linkedlist = new LinkedList<int>(new int[] { });
+                    collections.Two(linkedlist);
+                    break;
+                //3
+                case ("3"):
+                    HashSet<string> menu = new HashSet<string> { "Пицца", "Салат", "Суп", "Бургер", "Паста", "Суши","Багет","Шаурма"};
+                    List<HashSet<string>> orders = new List<HashSet<string>>()
+        {
+            new HashSet<string> { "Пицца", "Салат", "Суп","Шаурма" },
+            new HashSet<string> { "Пицца", "Паста", "Суп", "Бургер" },
+            new HashSet<string> { "Пицца", "Салат", "Суп", "Багет", "Суши" },
+        };
+                    collections.Three(menu, orders);
+                    break;
+                //4
+                case ("4"):
+                    Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+                    string Input = "note1.exe";
+                    try
+                    {
+                        StreamReader check = new StreamReader(Input);
+                    }
+                    catch
+                    {
+                        Console.Write("Такой путь не может существовать");
+                        break;
+                    }
+                    HashSet<char> sogl = new HashSet<char>("бвгджзйклмнпрстфхцчшщ");
+
+                    HashSet<char> otvet = new HashSet<char>();
+                    otvet = collections.MMM(Input, sogl);
+                    Console.Write("Уникальные согласные : ");
+                    foreach (var item in otvet)
+                    {
+                        Console.Write(item + " ");
+                    }
+                    Console.WriteLine();
+                    break;
+            }
+        }
+    }
+}
---
+++ Sharp4/collections.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharp4
+{
+    internal class collections
+    {
+        public static List<int> One(List<int> list1, List<int> list2)
+        {
+
+            if (list1[0] < list1[1])
+            {
+                int i = 0, j = 0;
+                while (j < list2.Count)
+                {
+                    while (i < list1.Count && list1[i] <= list2[j])
+                    {
+                        i++;
+                    }
+                    list1.Insert(i, list2[j]);
+                    j++;
+                }
+                return list1;
+            }
+            else
+            {
+                int i = 0, j = 0;
+                while (j < list2.Count)
+                {
+                    while (i < list1.Count && list1[i] >= list2[j])
+                    {
+                        i++;
+                    }
+                    list1.Insert(i, list2[j]);
+                    j++;
+                }
+                return list1;
+            }
+        }
+        public static void Two(LinkedList<int> linkedlist)
+        {
+            int count = 0;
+            if (linkedlist.Count < 3)
+            {
+                Console.WriteLine('0');
+            }
+            else
+            {
+                var cur = linkedlist.First.Next;
+                while (cur.Next != null)
+                {
+                    if (cur.Previous.Value == cur.Next.Value)
+                    {
+                        count++;
+                    }
+                    cur = cur.Next;
+                }
+                Console.WriteLine(count);
+            }
+        }
+        public static void Three(HashSet<string> menu, List<HashSet<string>> orders)
+        {
+            HashSet<string> all = new HashSet<string>(orders[0]);
+            foreach (var item in orders)
+            {
+                all.IntersectWith(item);
+            }
+            HashSet<string> some = new HashSet<string>();
+            foreach (var item in orders)
+            {
+                some.UnionWith(item);
+            }
+            HashSet<string> none = new HashSet<string>(menu);
+            none.ExceptWith(some);
+            Console.Write("Меню : ");
+            foreach (var item in menu)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            Console.Write("Позиции которые заказали все : ");
+            foreach (var item in all)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            Console.Write("Позиции которые заказали хотя бы раз : ");
+            foreach (var item in some)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            Console.Write("Позиции которые ни разу не заказали : ");
+            foreach (var item in none)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+        public static HashSet<char> MMM(string In, HashSet<char> sogl)
+        {
+            StreamReader sr = new StreamReader(In, Encoding.GetEncoding("windows-1251"));
+            string line = sr.ReadLine();
+            HashSet<char> single = new HashSet<char>();
+            HashSet<char> multi = new HashSet<char>();
+            while (line != null)
+            {
+                line = line.ToLower();
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if (sogl.Contains(line[i]))
+                    {
+                        if (!(single.Contains(line[i])))
+                        {
+                            single.Add(line[i]);
+                        }
+                        else
+                        {
+                            multi.Add(line[i]);
+                        }
+                    }
+                }
+                line = sr.ReadLine();
+            }
+            single.ExceptWith(multi);
+            sr.Close();
+            return single;
+        }
+    }
+}
